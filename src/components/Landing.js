522d8cbe560196b47b5bffaba9d27459c712e28e
modehans <monicaglezsanch@gmail.com>
2022-08-24 20:38:51 +0200

    Change class by classname on Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,43 +3,43 @@ import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <main ClassName="main">
-      <div ClassName="main__colcen">
-        <section ClassName="main__section">
+    <main className="main">
+      <div className="main__colcen">
+        <section className="main__section">
           <img
-            ClassName="main__section--img"
+            className="main__section--img"
             src="./assets/images/logo-awesome-profile-cards.png"
             alt="imagen logo"
             title="logo"
           />
         </section>
 
-        <section ClassName="main__section">
-          <h1 ClassName="main__section--title">Crea tu tarjeta de visita</h1>
-          <p ClassName="main__section--paragraph">
+        <section className="main__section">
+          <h1 className="main__section--title">Crea tu tarjeta de visita</h1>
+          <p className="main__section--paragraph">
             Crea mejores contactos profesionales de forma fácil y cómoda
           </p>
         </section>
 
-        <section ClassName="main__section--wrapper">
-          <div ClassName="main__icon">
-            <i ClassName="fa-solid fa-object-ungroup icon-index-main"></i>
-            <p ClassName="main__icon--paragraph">Diseña</p>
+        <section className="main__section--wrapper">
+          <div className="main__icon">
+            <i className="fa-solid fa-object-ungroup icon-index-main"></i>
+            <p className="main__icon--paragraph">Diseña</p>
           </div>
 
-          <div ClassName="main__icon">
-            <i ClassName="fa-solid fa-keyboard icon-index-main"></i>
-            <p ClassName="main__icon--paragraph">Rellena</p>
+          <div className="main__icon">
+            <i className="fa-solid fa-keyboard icon-index-main"></i>
+            <p className="main__icon--paragraph">Rellena</p>
           </div>
 
-          <div ClassName="main__icon">
-            <i ClassName="fa-solid fa-share-nodes icon-index-main"></i>
-            <p ClassName="main__icon--paragraph">Comparte</p>
+          <div className="main__icon">
+            <i className="fa-solid fa-share-nodes icon-index-main"></i>
+            <p className="main__icon--paragraph">Comparte</p>
           </div>
         </section>
 
-        <section ClassName="main__section main__last">
-          <div ClassName="main__link--div">
+        <section className="main__section main__last">
+          <div className="main__link--div">
             <Link className="main__link--link" to="/card">
               Comenzar
             </Link>
